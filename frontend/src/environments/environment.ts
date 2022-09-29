c242a7daf7b62290e1561eb66163fc1362684eaa
Omotola Macaulay <tolamacaulay@gmail.com>
2022-09-29 18:57:14 +0100

    Add get endpoints and fix postman errors

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: 'http://127.0.0.1:5001', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'access-identification.us', // the auth0 domain prefix
+    audience: 'coffee', // the audience set for the auth0 app
+    clientId: '9NutgdXVGrzqsW392yJrJ665lPmOx6nO', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
